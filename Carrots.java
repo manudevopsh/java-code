88b04c4107729c30e5226b0800be2399c77194de
Surya <bhnsurya200@gmail.com>
2020-01-31 15:58:58 -0600

    Add files via upload

---
+++ Carrots.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Carrots {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		int p= sc.nextInt();
+		for(int i=0;i<n;i++) {
+			sc.next();
+		}
+		System.out.println(p);
+	}
+}
+
