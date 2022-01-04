1c05a767c18348b09d7da1e10d90a102a116323b
Surya <bhnsurya200@gmail.com>
2022-01-04 14:43:33 -0600

    https://open.kattis.com/problems/ratingproblems
    https://open.kattis.com/problems/ratingproblems

---
+++ RatingProblems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class RatingProblems
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int n = int.Parse(values[0]);
+            int k = int.Parse(values[1]);
+            int total = 0;
+            for(int i =0;i<k; i++)
+            {
+                total += int.Parse(Console.ReadLine());
+
+            }
+            float min = (float)(total + (n - k) * (-3)) / (float)n;
+            float max = (float)(total + (n - k) * (3)) / (float)n;
+            Console.WriteLine(min + " " + max);
+            
+
+        }
+    }
+}
