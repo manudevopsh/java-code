fcf2d55c9442181d99ce7574ada1f37c6c7ebd1f
Surya <bhnsurya200@gmail.com>
2022-01-04 15:30:25 -0600

    https://open.kattis.com/problems/nsum
    https://open.kattis.com/problems/nsum

---
+++ NSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class NumberFun
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] x = Console.ReadLine().Split(' ');
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                sum += Convert.ToInt32(x[i]);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
