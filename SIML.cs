7310c20a766f37682a42f1609e5b8fe3032af61c
Surya <bhnsurya200@gmail.com>
2022-01-04 16:07:31 -0600

    https://open.kattis.com/problems/smil
    https://open.kattis.com/problems/smil

---
+++ SIML.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class SIML
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] inputChar = input.ToCharArray();
+            for(int i = 0; i < inputChar.Length; i++)
+            {
+                if ((inputChar[i] == ':')|| (inputChar[i] == ';'))
+                {
+                    if((i<inputChar.Length-1) && (inputChar[i+1] == ')'))
+                    {
+                        Console.WriteLine(i);
+                    }
+                    else if((i < inputChar.Length-2) && (inputChar[i + 1] == '-') && (inputChar[i + 2] == ')'))
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
