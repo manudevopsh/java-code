830178175e992dadbc3f40810e1558bef809667a
Surya <bhnsurya200@gmail.com>
2022-01-04 14:22:49 -0600

    https://open.kattis.com/problems/sorttwonumbers
    https://open.kattis.com/problems/sorttwonumbers

---
+++ SortTwoNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class SortTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int x = int.Parse(values[0]);
+            int y = int.Parse(values[1]);
+            if (y > x)
+            {
+                Console.WriteLine(x +" "+ y);
+            }
+            else
+            {
+                Console.WriteLine(y + " " + x);
+            }
+            
+
+        }
+    }
+}
