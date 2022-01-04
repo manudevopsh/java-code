a1430dc21a3798c0825d16580095144dbee6435e
Surya <bhnsurya200@gmail.com>
2022-01-04 13:45:55 -0600

    https://open.kattis.com/problems/oddecho
    https://open.kattis.com/problems/oddecho

---
+++ OddEcho.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class OddEcho
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] input = new string[n];
+            for(int i = 0; i < n; i++)
+            {
+                input[i] = Console.ReadLine();
+            }
+            for(int i = 0; i < n; i = i + 2)
+            {
+                Console.WriteLine(input[i]);
+            }
+            
+        }
+    }
+}
