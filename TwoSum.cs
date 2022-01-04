ad802f6a358a84cefbaf0603e0b95681d7c2efb5
Surya <bhnsurya200@gmail.com>
2022-01-04 14:17:42 -0600

    https://open.kattis.com/problems/twosum
    https://open.kattis.com/problems/twosum

---
+++ TwoSum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class TwoSum
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int x = int.Parse(values[0]);
+            int y = int.Parse(values[1]);
+
+            Console.WriteLine(x + y);
+
+        }
+    }
+}
