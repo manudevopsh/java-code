449e87db58ae4dd8ea82eec5a6c6e92a896f74ab
Surya <bhnsurya200@gmail.com>
2022-01-04 15:19:09 -0600

    https://open.kattis.com/problems/numberfun
    https://open.kattis.com/problems/numberfun

---
+++ NumberFun.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class NumberFun
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                int a = int.Parse(values[0]);
+                int b = int.Parse(values[1]);
+                int c = int.Parse(values[2]);
+                if(((a+b)==c)|| (Math.Abs(a - b) == c)|| ((a * b) == c)|| ((a + b) == c)|| (((float)(a) / (float)(b)) == c)|| (((float)(b) / (float)(a)) == c))
+                {
+                    Console.WriteLine("Possible");
+                }
+                else
+                {
+                    Console.WriteLine("Impossible");
+                }
+            }
+            
+            
+
+        }
+    }
+}
