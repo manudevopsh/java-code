59cee5dbda66fd017ce8bcac287e3e5c2dd10375
Surya <bhnsurya200@gmail.com>
2022-01-04 15:52:00 -0600

    https://open.kattis.com/problems/echoechoecho
    https://open.kattis.com/problems/echoechoecho

---
+++ EchoEchoEcho.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class FYI
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input + " " + input + " " + input);
+        }
+    }
+}
