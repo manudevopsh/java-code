2d2c0a28bd3a742f3280d54a4af6c273919a6cff
Surya <bhnsurya200@gmail.com>
2022-01-04 15:48:24 -0600

    https://open.kattis.com/problems/fyi
    https://open.kattis.com/problems/fyi

---
+++ FYI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class FYI
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int areacode = n / 10000;
+            if(areacode == 555)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
