1257bcac9945f1a11f1a411c8a586c55c0d057b9
Surya <bhnsurya200@gmail.com>
2022-01-04 13:35:24 -0600

    https://open.kattis.com/problems/digitswap
    https://open.kattis.com/problems/digitswap

---
+++ DigitSwap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class DigitSwap
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int num1 = input % 10;
+            int num2 = input / 10;
+            Console.WriteLine(num1+""+num2);
+        }
+    }
+}
