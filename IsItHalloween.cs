b7255d6c4e4d2e40df8f0e836854c978cc50fcaf
Surya <bhnsurya200@gmail.com>
2022-01-04 15:41:42 -0600

    https://open.kattis.com/problems/isithalloween
    https://open.kattis.com/problems/isithalloween

---
+++ IsItHalloween.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class IsItHalloween
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] x = Console.ReadLine().Split(' ');
+            string month = x[0];
+            int day = int.Parse(x[1]);
+            if(((month =="OCT")&&(day == 31))|| ((month == "DEC") && (day == 25))){
+                Console.WriteLine("yup");
+            }
+            else
+            {
+                Console.WriteLine("nope");
+            }
+        }
+    }
+}
