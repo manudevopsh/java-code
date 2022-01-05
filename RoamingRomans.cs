580fbb888f8c5e8bb50cee29b5cf05c2daa80329
Surya <bhnsurya200@gmail.com>
2022-01-05 13:11:23 -0600

    https://open.kattis.com/problems/romans

---
+++ RoamingRomans.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class RoamingRomans
+    {
+        static void Main(string[] args)
+        {
+            float x= float.Parse(Console.ReadLine());
+            float miles = (5280000 * x) / 4854;
+            Console.WriteLine(Decimal.Round((decimal)miles));
+        }
+    }
+}
