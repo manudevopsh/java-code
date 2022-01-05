ec01d7b62ad6145e8ae6879aecb99299f5bd5cea
Surya <bhnsurya200@gmail.com>
2022-01-05 09:52:09 -0600

    Add files via upload
    https://open.kattis.com/problems/triarea

---
+++ TriangleArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int h = int.Parse(values[0]);
+            int b = int.Parse(values[1]);
+            float area = (float)(b * h) / 2;
+            Console.WriteLine(area);
+
+        }
+    }
+}
