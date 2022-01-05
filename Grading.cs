353fe6f4e1aef8dd131c9de15cf46bf267da4f99
Surya <bhnsurya200@gmail.com>
2022-01-05 10:00:02 -0600

    https://open.kattis.com/problems/grading

---
+++ Grading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class Grading
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int a = int.Parse(values[0]);
+            int b = int.Parse(values[1]);
+            int c = int.Parse(values[2]);
+            int d = int.Parse(values[3]);
+            int e = int.Parse(values[4]);
+            int score = int.Parse(Console.ReadLine());
+            if (score >= a)
+            {
+                Console.WriteLine("A");
+            }else if(score >= b)
+            {
+                Console.WriteLine("B");
+            }
+            else if (score >= c)
+            {
+                Console.WriteLine("C");
+            }
+            else if (score >= d)
+            {
+                Console.WriteLine("D");
+            }
+            else if (score >= e)
+            {
+                Console.WriteLine("E");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+
+
+        }
+    }
+}
