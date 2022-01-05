b0ebbdb4b31ebabfebbb1603c562e2fe224831db
Surya <bhnsurya200@gmail.com>
2022-01-05 10:14:08 -0600

    https://open.kattis.com/problems/pyramids

--- Grading.cs
+++ Grading.cs
@@ -6,37 +6,17 @@ namespace KattisProblems
     {
         static void Main(string[] args)
         {
-            string[] values = Console.ReadLine().Split(' ');
-            int a = int.Parse(values[0]);
-            int b = int.Parse(values[1]);
-            int c = int.Parse(values[2]);
-            int d = int.Parse(values[3]);
-            int e = int.Parse(values[4]);
-            int score = int.Parse(Console.ReadLine());
-            if (score >= a)
-            {
-                Console.WriteLine("A");
-            }else if(score >= b)
-            {
-                Console.WriteLine("B");
-            }
-            else if (score >= c)
-            {
-                Console.WriteLine("C");
-            }
-            else if (score >= d)
-            {
-                Console.WriteLine("D");
-            }
-            else if (score >= e)
-            {
-                Console.WriteLine("E");
-            }
-            else
-            {
-                Console.WriteLine("F");
-            }
 
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            int height = 0;
+            while (n>=i*i)
+            {
+                n = n - i * i;
+                i = i + 2;
+                height++;
+            }
+            Console.WriteLine(height);
 
         }
     }
