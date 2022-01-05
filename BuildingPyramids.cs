595f570631669be04cb96abb430444a101219db3
Surya <bhnsurya200@gmail.com>
2022-01-05 10:15:42 -0600

    https://open.kattis.com/problems/pyramids

---
+++ BuildingPyramids.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class Grading
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            int height = 0;
+            while (n>=i*i)
+            {
+                n = n - i * i;
+                i = i + 2;
+                height++;
+            }
+            Console.WriteLine(height);
+
+        }
+    }
+}
