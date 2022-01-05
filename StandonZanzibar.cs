ddaeb5e6897e2276db5ad362b52349c275a7d6a5
Surya <bhnsurya200@gmail.com>
2022-01-05 10:47:40 -0600

    https://open.kattis.com/problems/zanzibar

---
+++ StandonZanzibar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class StandonZanzibar
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                int j = 0;
+                int imported = 0;
+                while (int.Parse(values[j+1]) != 0)
+                {
+                    int difference = int.Parse(values[j + 1]) - int.Parse(values[j]) * 2;
+                    if (difference > 0)
+                    {
+                        imported += difference;
+                    }
+                    j++;
+                }
+                Console.WriteLine(imported);
+            }
+        }
+    }
+}
