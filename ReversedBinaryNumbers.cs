d6ac9e6976de882d469323af1ca12155e8eb72a7
Surya <bhnsurya200@gmail.com>
2022-01-11 15:54:36 -0600

    https://open.kattis.com/problems/reversebinary

---
+++ ReversedBinaryNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class ReversedBinaryNumbers
+    {
+        static void Main(string[] args)
+        {
+            int input= int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(input, 2);
+            char[] cArray = binary.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            int value = Convert.ToInt32(reverse, 2);
+            Console.WriteLine(value);
+        }
+    }
+}
