d094f1bb578cdc31b5f315d2054e7c2c95c2ae33
Surya <bhnsurya200@gmail.com>
2022-01-12 11:42:33 -0600

    https://open.kattis.com/problems/provincesandgold

---
+++ ProvincesAndGold.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class ProvincesAndGold
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int g = int.Parse(values[0]);
+            int s = int.Parse(values[1]);
+            int c = int.Parse(values[2]);
+            int cost = g * 3 + s * 2 + c;
+            if (cost >= 8)
+            {
+                Console.WriteLine("Province or Gold");
+            }
+            else if (cost >= 6)
+            {
+                Console.WriteLine("Duchy or Gold");
+            }
+            else if (cost >= 5)
+            {
+                Console.WriteLine("Duchy or Silver");
+            }
+            else if (cost >= 3)
+            {
+                Console.WriteLine("Estate or Silver");
+            }
+            else if (cost >= 2)
+            {
+                Console.WriteLine("Estate or Copper");
+            }
+            else
+            {
+                Console.WriteLine("Copper");
+            }
+        }
+    }
+}
